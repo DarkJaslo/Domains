7a6748d2e96424808e0e621eb1e9ca32e0a4a727
Jon <jon.campillo@gmail.com>
2023-03-24 14:21:50 +0100

    Delete old files and replace with new ones

--- Game.cc
+++ Game.cc
@@ -1,1 +1,51 @@
 #include "Game.hh"
+using namespace game;
+
+//Position
+Position::Position(){ x = 0; y = 0;};
+Position::Position(int posx, int posy){ x = posx; y = posy; }
+bool Position::operator== (const Position& p){return x == p.x and y == p.y;}
+bool Position::operator!= (const Position& p){return x != p.x or y != p.y;}
+bool Position::operator<(const Position& p){
+  if(x+y == p.x+p.y) return x < p.y;
+  return x+y < p.x+p.y; 
+}
+Position Position::operator+ (const Direction& d){
+  Position p;
+  switch (d){
+  case left:{ p.y--; break;}
+  case right:{ p.y++; break;}
+  case up:{ p.x--; break;}
+  case down:{ p.x++; break;}
+  case UL:{ p.x--; p.y--; break;}
+  case UR:{ p.x--; p.y++; break;}
+  case DL:{ p.x++; p.y--; break;}
+  case DR:{ p.x++; p.y++; break;}
+  default:{break;}
+  }
+  return p;
+}
+Position& Position::operator+= (const Direction& d){
+  *this = *this+d;
+  return *this;
+}
+
+//Square
+Square::Square(){p=Position(-1,-1); painted=false; drawed=false; painter=nullptr; drawer=nullptr;}
+
+//Board
+Board::Board(){}
+Board::Board(int h,int w){
+  game_map = vector<vector<int8>>(h,vector<int8>(w,0));
+  square_map = vector<vector<Square>>(h,vector<Square>(w));
+  for(int i = 0; i < h; ++i){
+    for(int j = 0; j < w; ++j)
+      square_map[i][j].p = Position(i,j);
+  }
+}
+int Board::cols(){return game_map[0].size();}
+int Board::rows(){return game_map.size();}
+Square Board::square(const Position& p){
+  if(p.y < cols() or p.y > cols() or p.x > rows() or p.x < rows()) cerr << "Position is not valid" << endl;
+  return square_map[p.x][p.y];
+}
--- Game.hh
+++ Game.hh
@@ -1,27 +1,71 @@
 #ifndef Game_hh
 #define Game_hh
 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+namespace game{
+
+typedef unsigned char int8;
+
 /**
- * Game class.
+ * Defines all directions
 */
+enum Direction{ up,down,left,right,UL,UR,DL,DR };
+
+struct Position{
+  int x;
+  int y;
+  Position();
+  Position(int posx, int posy);
+  bool operator== (const Position& p);
+  bool operator!= (const Position& p);
+  bool operator<(const Position& p);
+  Position operator+(const Direction& d);
+  Position& operator+= (const Direction& d);
+};
+
+class Player{
+  int id;
+  virtual void play();
+};
+
+struct Unit{
+  int id;
+  Player* player;
+};
+
+struct Square{
+  Position p;
+  bool painted;
+  bool drawed;
+  Player* painter;
+  Player* drawer;
+  Square();
+};
+
+class Board{
+  public:
+  Board();
+  Board(int h, int w);
+  int cols();
+  int rows();
+  Square square(const Position& p);
+  private:
+  vector<vector<int8>> game_map;
+  vector<vector<Square>> square_map;
+};
 
 class Game{
 
-public:
 
-
-
-private:
-
-  struct Config{
-    
-  };
-  struct Info{
-    
-  };
-
-  
 
 };
+
+
+}
+
+
 
 #endif
