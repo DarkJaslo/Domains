cb5e0fe52de5c248721233d4cf77e193a0ff672d
Jon <jon.campillo@gmail.com>
2023-05-04 23:44:15 +0200

    to get 4 different players

---
+++ AIPlayer4.cc
@@ -0,0 +1,15 @@
+#include "Player.hh"
+
+#define PLAYER_NAME Player4
+
+struct PLAYER_NAME : public Player{
+  static Player* factory(){
+    return new PLAYER_NAME;
+  }
+
+  virtual void play(){
+    //move(2,Direction::null);
+  }
+};
+
+REGISTER_PLAYER(PLAYER_NAME);
