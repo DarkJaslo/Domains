c1d04987430cf2fe6caddf3a295b4fb2d145aec1
Jon <jon.campillo@gmail.com>
2023-05-07 16:28:40 +0200

    new bonus system and ability testing

--- AIPlayer1.cc
+++ AIPlayer1.cc
@@ -22,11 +22,26 @@ struct PLAYER_NAME : public Player{
     }
     cerr << endl;
 
-    if(round() == 0){ //first round, few local options
+    /*if(round() == 0){ //first round, few local options
       move(u[0],Direction::up);
     }
     else if(round() == 1){  //same here
       move(u[0],Direction::UL);
+    }*/
+
+    Position bonusPos = Position(12,20);
+
+    if(unit(u[0]).upgraded()){
+      cerr << "UNIT IS UPGRADED" << endl;
+      ability(u[0]);
+    }
+    else{
+      if(unit(u[0]).position().y < bonusPos.y){
+        move(u[0],Direction::right);
+      }
+      else if(unit(u[0]).position().x > bonusPos.x){
+        move(u[0],Direction::up);
+      }      
     }
 
     
