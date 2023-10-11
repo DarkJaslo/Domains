9c05adde863debea67b19d2b3eafced2a0e58dd6
Jon <jon.campillo@gmail.com>
2023-10-11 22:23:56 +0200

    functional but quite bad (too many idle units)

--- src/bots/AIXploid.cc
+++ src/bots/AIXploid.cc
@@ -1,4 +1,5 @@
 #include "../game/Player.hh"
+#include <functional>
 
 #define PLAYER_NAME Xploid
 using namespace std;
@@ -6,6 +7,102 @@ using namespace std;
 struct PLAYER_NAME : public Player{
 
 static Player* factory(){ return new PLAYER_NAME; }
+
+/*
+
+	CLASSES
+
+*/
+
+class PositionSet{
+public:
+  enum State{OUTSIDE,QUEUED,INSIDE};
+  PositionSet(){
+    _container = Matrix<int8_t>(50,50);
+    clear();
+  }
+  bool queued(Position p) const{
+    return _container[p] == State::QUEUED;
+  }
+  bool contains(Position p) const{
+    return _container[p] == State::INSIDE;
+  }
+  void queue(Position p){
+    _container[p] = State::QUEUED;
+  }
+  void add(Position p){
+    _container[p] = State::INSIDE;
+  }
+  void remove(Position p){
+    _container[p] = State::OUTSIDE;
+  }
+  void clear(){
+    for(int i = 0; i < 50; ++i){
+      for(int j = 0; j < 50; ++j){
+        _container[Position(i,j)] = State::OUTSIDE;
+      }
+    }
+  }
+  void print(){
+    std::cerr << "printing contents..." << std::endl;
+    for(int i = 0; i < 50; ++i){
+      for(int j = 0; j < 50; ++j){
+        Position p(i,j);
+        if(_container[p] != State::OUTSIDE){
+          std::cerr << i << "," << j << " ";
+        }
+      }
+    }
+    std::cerr << std::endl;
+  }
+private:
+  Matrix<int8_t> _container;  
+};
+
+class Targets{
+public:
+	Targets(){
+		info = Matrix<int8_t>(rows(),cols());
+	}
+
+	void clear()
+	{
+		for(int i = 0; i < rows(); ++i)
+			for(int j = 0; j < cols(); ++j)
+				info[Position(i,j)] = 0;
+	}
+	void target(Position p){ info[p]++; }
+	bool targeted(Position p) const { return info[p] > 0; }
+	int  targets(Position p) const{ return info[p]; }
+
+private:
+	Matrix<int8_t> info;
+};
+
+struct BFSNode{
+	BFSNode(){ dist = -1; pos = Position(-1,-1); }
+	BFSNode(int dist, Position pos) : dist(dist), pos(pos) {}
+	int dist;
+	Position pos;
+};
+
+struct BFSInfo{
+	BFSInfo(){}
+	queue<BFSNode> q;
+	PositionSet v;
+};
+
+struct MyOrder{
+	MyOrder(){prio = 100;}
+	MyOrder(const Order& order, int priority) : ord(order), prio(priority){}
+	Order ord;
+	int prio;
+
+	bool operator<(const MyOrder& other)const
+	{
+		return prio < other.prio;
+	}
+};
 
 /*
 	Opening moves
@@ -36,9 +133,24 @@ const vector<Direction> DIRS_STRAIGHT = {Direction::left, Direction::right, Dire
 const vector<Direction> DIRS_ALL = {Direction::left, Direction::right, Direction::up, Direction::down,
                                 Direction::UL, Direction::UR, Direction::DL, Direction::DR};
 vector<Direction> DIRS_MAP; //So directions work as if you were down-left
-Position startingPos;
 
+const int PRIO_MAX = 110;
+const int PRIO_FETCH = 50;
+
+const int MAX_BFS = 12;
+const int MAX_TARGETS = 2;
+
+
+/*
+	General structures
+*/
+Position startingPos;
 vector<int> myUnits;
+vector<bool> availableUnits;
+vector<int> commitedUnits;
+
+Targets targets;
+vector<MyOrder> orders;
 
 void play()
 {
@@ -46,13 +158,23 @@ void play()
 	{
 		init();
 	}
+	else update();
 
-	if(round() < OPENING_ROUNDS)
-	{
-		doOpeningMoves();
-		return;
-	}
+	doOpeningMoves();
 	//Else
+
+	if(round() <= OPENING_ROUNDS) return;
+
+	for(int i = 0; i < myUnits.size(); ++i)
+	{
+		if(unit(myUnits[i]).upgraded())
+		{
+			orders[i] = MyOrder(Order(i,Direction::null,OrderType::ability),0);
+			availableUnits[i] = false;
+		}
+	}
+
+	layeredBFS(1,MAX_BFS);
 
 	/*
 	1.Handle critical stuff
@@ -67,10 +189,31 @@ void play()
 	To remaining units:
 
 	3.Try to draw (commitment, not free anymore)
-
-	
 	
 	*/
+
+	priority_queue<MyOrder> orderQueue;
+	for(const MyOrder& ord : orders)
+	{
+		orderQueue.push(ord);
+	}
+
+	while(not orderQueue.empty())
+	{
+		MyOrder ord = orderQueue.top();
+		orderQueue.pop();
+
+		if(ord.ord.type == OrderType::movement)
+		{
+			move(myUnits[ord.ord.unitId],ord.ord.dir);
+		}
+		else if(ord.ord.type == OrderType::attack){
+			attack(myUnits[ord.ord.unitId],ord.ord.dir);
+		}
+		else if(ord.ord.type == OrderType::ability){
+			ability(myUnits[ord.ord.unitId]);
+		}
+	}
 
 
 } //play() end
@@ -79,6 +222,14 @@ void init()
 {
 	initOpeningMoves();
 	initDirections();
+	myUnits = units(me());
+	commitedUnits = std::vector<int>(myUnits.size(),0);
+	targets = Targets();
+	targets.clear();
+	orders = vector<MyOrder>(myUnits.size());
+	for(int i = 0; i < orders.size(); ++i){
+		orders[i].ord = Order(i,Direction::null,OrderType::movement);
+	}
 }
 
 void initOpeningMoves()
@@ -286,94 +437,235 @@ void initDirections(){
 
 void doOpeningMoves()
 {
-	//Unit 1
-	if(round() >= 3){
-		if(round() == 3){
-			fillSpecialDirections(Position(49,2),unit(myUnits[1]).position(),UNIT1SPECIAL[0],UNIT1SPECIAL[1]);
-			move(myUnits[1],DIRS_MAP[UNIT1SPECIAL[0]]);
+	if(round() < OPENING_ROUNDS)
+	{
+			//Unit 1
+		if(round() >= 3){
+			if(round() == 3){
+				fillSpecialDirections(Position(49,2),unit(myUnits[1]).position(),UNIT1SPECIAL[0],UNIT1SPECIAL[1]);
+				move(myUnits[1],DIRS_MAP[UNIT1SPECIAL[0]]);
+			}
+			else if(round() == 4){  move(myUnits[1],DIRS_MAP[UNIT1SPECIAL[1]]);  }
+			else{  move(myUnits[1],DIRS_MAP[UNIT1OPENING[round()-5]]);  }
 		}
-		else if(round() == 4){  move(myUnits[1],DIRS_MAP[UNIT1SPECIAL[1]]);  }
-		else{  move(myUnits[1],DIRS_MAP[UNIT1OPENING[round()-5]]);  }
-	}
 
-	//Unit 2
-	if(round() >= 6){
-		if(round() == 6){
-			fillSpecialDirections(Position(47,1),unit(myUnits[2]).position(),UNIT2SPECIAL[0],UNIT2SPECIAL[1]);
-			move(myUnits[2],DIRS_MAP[UNIT2SPECIAL[0]]);
+		//Unit 2
+		if(round() >= 6){
+			if(round() == 6){
+				fillSpecialDirections(Position(47,1),unit(myUnits[2]).position(),UNIT2SPECIAL[0],UNIT2SPECIAL[1]);
+				move(myUnits[2],DIRS_MAP[UNIT2SPECIAL[0]]);
+			}
+			else if(round() == 7){  move(myUnits[2],DIRS_MAP[UNIT2SPECIAL[1]]);  }
+			else{  move(myUnits[2],DIRS_MAP[UNIT2OPENING[round()-8]]);  }
 		}
-		else if(round() == 7){  move(myUnits[2],DIRS_MAP[UNIT2SPECIAL[1]]);  }
-		else{  move(myUnits[2],DIRS_MAP[UNIT2OPENING[round()-8]]);  }
-	}
 
-	//Unit 3
-	if(round() >= 9){
-		if(round() == 9){
-			fillSpecialDirections(Position(47,2),unit(myUnits[3]).position(),UNIT3SPECIAL[0],UNIT3SPECIAL[1]);
-			move(myUnits[3],DIRS_MAP[UNIT3SPECIAL[0]]);
+		//Unit 3
+		if(round() >= 9){
+			if(round() == 9){
+				fillSpecialDirections(Position(47,2),unit(myUnits[3]).position(),UNIT3SPECIAL[0],UNIT3SPECIAL[1]);
+				move(myUnits[3],DIRS_MAP[UNIT3SPECIAL[0]]);
+			}
+			else if(round() == 10){  move(myUnits[3],DIRS_MAP[UNIT3SPECIAL[1]]);  }
+			else{  move(myUnits[3],DIRS_MAP[UNIT3OPENING[round()-11]]);  }
 		}
-		else if(round() == 10){  move(myUnits[3],DIRS_MAP[UNIT3SPECIAL[1]]);  }
-		else{  move(myUnits[3],DIRS_MAP[UNIT3OPENING[round()-11]]);  }
-	}
 
-	//Unit 4
-	if(round() >= 12){
-		if(round() == 12){
-			fillSpecialDirections(Position(47,0),unit(myUnits[4]).position(),UNIT4SPECIAL[0],UNIT4SPECIAL[1]);
-			move(myUnits[4],DIRS_MAP[UNIT4SPECIAL[0]]);
+		//Unit 4
+		if(round() >= 12){
+			if(round() == 12){
+				fillSpecialDirections(Position(47,0),unit(myUnits[4]).position(),UNIT4SPECIAL[0],UNIT4SPECIAL[1]);
+				move(myUnits[4],DIRS_MAP[UNIT4SPECIAL[0]]);
+			}
+			else if(round() == 13){  move(myUnits[4],DIRS_MAP[UNIT4SPECIAL[1]]);  }
+			else{  move(myUnits[4],DIRS_MAP[UNIT4OPENING[round()-14]]);  }
 		}
-		else if(round() == 13){  move(myUnits[4],DIRS_MAP[UNIT4SPECIAL[1]]);  }
-		else{  move(myUnits[4],DIRS_MAP[UNIT4OPENING[round()-14]]);  }
-	}
 
-	//Unit 5
-	if(round() >= 15){
-		if(round() == 15){
-			fillSpecialDirections(Position(48,2),unit(myUnits[5]).position(),UNIT5SPECIAL[0],UNIT5SPECIAL[1]);
-			move(myUnits[5],DIRS_MAP[UNIT5SPECIAL[0]]);
+		//Unit 5
+		if(round() >= 15){
+			if(round() == 15){
+				fillSpecialDirections(Position(48,2),unit(myUnits[5]).position(),UNIT5SPECIAL[0],UNIT5SPECIAL[1]);
+				move(myUnits[5],DIRS_MAP[UNIT5SPECIAL[0]]);
+			}
+			else if(round() == 16){  move(myUnits[5],DIRS_MAP[UNIT5SPECIAL[1]]);  }
+			else{  move(myUnits[5],DIRS_MAP[UNIT5OPENING[round()-17]]);  }
 		}
-		else if(round() == 16){  move(myUnits[5],DIRS_MAP[UNIT5SPECIAL[1]]);  }
-		else{  move(myUnits[5],DIRS_MAP[UNIT5OPENING[round()-17]]);  }
-	}
 
-	//Unit 6
-	if(round() >= 18){
-		if(round() == 18){
-			move(myUnits[6],DIRS_MAP[virtualPos(unit(myUnits[6]).position()).to(Position(48,1))]);
+		//Unit 6
+		if(round() >= 18){
+			if(round() == 18){
+				move(myUnits[6],DIRS_MAP[virtualPos(unit(myUnits[6]).position()).to(Position(48,1))]);
+			}
+			else if(round() == 19){
+				//attack bubble
+				Position aux = unit(myUnits[6]).position();
+				Position targetPos = meleeTarget(aux,true,playerHasBubble);
+				if(targetPos != Position(-1,-1)){
+					attack(myUnits[6],aux.to(targetPos));
+				}
+			}
+			else{  move(myUnits[6],DIRS_MAP[UNIT6OPENING[round()-20]]);  }
 		}
-		else if(round() == 19){
-			//attack bubble
-			Position aux = unit(myUnits[6]).position();
-			Position targetPos = meleeTarget(aux,true,playerHasBubble);
-			if(targetPos != Position(-1,-1)){
-				attack(myUnits[6],aux.to(targetPos));
+
+		//Unit 7
+		if(round() >= 21){
+			Position aux = virtualPos(unit(myUnits[7]).position());
+			if(aux.x < 47 or aux.y > 2){
+				move(myUnits[7],DIRS_MAP[Direction::null]);
+			}
+			else{
+				if(round() == 21){
+					move(myUnits[7],DIRS_MAP[virtualPos(unit(myUnits[7]).position()).to(Position(48,1))]);
+				}
+				else{  move(myUnits[7],DIRS_MAP[UNIT7OPENING[round()-22]]); }
 			}
 		}
-		else{  move(myUnits[6],DIRS_MAP[UNIT6OPENING[round()-20]]);  }
-	}
 
-	//Unit 7
-	if(round() >= 21){
-		Position aux = virtualPos(unit(myUnits[7]).position());
-		if(aux.x < 47 or aux.y > 2){
-			move(myUnits[7],DIRS_MAP[Direction::null]);
-		}
-		else{
-			if(round() == 21){
-				move(myUnits[7],DIRS_MAP[virtualPos(unit(myUnits[7]).position()).to(Position(48,1))]);
-			}
-			else{  move(myUnits[7],DIRS_MAP[UNIT7OPENING[round()-22]]); }
-		}
-	}
+		//Unit 0
+		move(myUnits[0],DIRS_MAP[UNIT0OPENING[round()]]);
 
-	//Unit 0
-	move(myUnits[0],DIRS_MAP[UNIT0OPENING[round()]]);
+		return;
+	}
+	else if(round() > OPENING_ROUNDS) return;
+
+	//Else,
+	//Very specific moves with a very specific order, don't touch
+    if(virtualPos(unit(myUnits[8]).position()) == Position(47,2)){
+      move(myUnits[8],DIRS_MAP[Direction::down]);
+    }
+    else if(virtualPos(unit(myUnits[9]).position()) == Position(47,2)){
+      if(virtualPos(unit(myUnits[8]).position()) == Position(48,2)){
+        move(myUnits[8],DIRS_MAP[Direction::down]);
+      }
+      move(myUnits[9],DIRS_MAP[Direction::down]);
+    }
+    move(myUnits[2],DIRS_MAP[Direction::down]);
+    move(myUnits[0],DIRS_MAP[Direction::left]);
+    move(myUnits[4],DIRS_MAP[Direction::down]);
+    move(myUnits[1],DIRS_MAP[Direction::left]);
 }
 
+void update()
+{	
+	updateUnits();
+	targets.clear();
+	orders = vector<MyOrder>(myUnits.size());
+	for(int i = 0; i < orders.size(); ++i){
+		orders[i].ord = Order(i,Direction::null,OrderType::movement);
+	}
+}
 
+void updateUnits()
+{
+	//Update commitments and available units
+	std::vector<int> newUnits = units(me());
+	std::vector<int> newCommitedUnits(newUnits.size(),0);
+	availableUnits = std::vector<bool>(newUnits.size(),true);
 
+	for(int& value : commitedUnits)
+		if(value > 0) --value;
+	
+	for(int i = 0; i < myUnits.size(); ++i)
+	{
+		if(commitedUnits[i] <= 0) continue;
 
+		int index = dichSearch(myUnits[i],newUnits);
+		if(index >= 0)
+		{
+			//Unit is there
+			newCommitedUnits[index] = commitedUnits[i];
+			availableUnits[index] = false;
+		}
+	}
 
+	myUnits = newUnits;
+	commitedUnits = newCommitedUnits;
+}
+
+void layeredBFS(int initialRadius, int maxRadius)
+{
+	int remainingUnits = availableUnits.size();
+	vector<bool> doneUnits = availableUnits;
+	for(int i = 0; i < doneUnits.size(); ++i){
+		doneUnits[i] = not doneUnits[i];
+		if(doneUnits[i]) remainingUnits--;
+	}
+
+	vector<BFSInfo> layeredInfo(myUnits.size());
+	for(int i = 0; i < layeredInfo.size(); ++i)
+	{
+		queueAdjacentPositions(BFSNode(0,unit(myUnits[i]).position()),layeredInfo[i].q,layeredInfo[i].v,(square(unit(myUnits[i]).position()).painter() == me()),me());
+	}
+
+	while(initialRadius < maxRadius and remainingUnits > 0)
+	{
+		for(int u = 0; u < myUnits.size(); ++u)
+		{
+			if(doneUnits[u]) continue;
+			if(layerOfBFS(u,initialRadius,layeredInfo[u]))
+			{
+				doneUnits[u] = true;
+				remainingUnits--;
+			}
+		}
+		++initialRadius;
+	}
+}
+
+bool layerOfBFS(int u, int radius, BFSInfo& info)
+{
+
+	if(info.q.empty()) return false;
+	do{
+		BFSNode n = info.q.front();
+		if(n.dist > radius) return false;
+		info.q.pop();
+
+		Position pos = n.pos;
+		Square sq = square(pos);
+
+		queueAdjacentPositions(n,info.q,info.v,/*sq.painter() == me()*/false,me());
+
+		//Evaluate conditions
+		bool finished = false;
+
+		
+		bool diag = sq.painter() == me();
+		if(sq.empty()) continue;
+		if(targets.targets(pos) >= MAX_TARGETS) continue;
+		if(sq.hasBubble())
+		{
+			Direction dir = bestDirectionFlex(pos,unit(myUnits[u]).position(),diag);
+			orders[u] = MyOrder(Order(u,dir,OrderType::movement),randomNumber(PRIO_FETCH,PRIO_FETCH+5));
+
+			targets.target(pos);
+			availableUnits[u] = false;
+			finished = true;
+		}
+		else if(sq.hasBonus())
+		{
+			Direction dir = bestDirectionFlex(pos,unit(myUnits[u]).position(),diag);
+			orders[u] = MyOrder(Order(u,dir,OrderType::movement),randomNumber(PRIO_FETCH,PRIO_FETCH+5));
+
+			targets.target(pos);
+			availableUnits[u] = false;
+			finished = true;
+		}
+		else if(sq.hasUnit() and sq.unit().player() != me() and sq.painter() == me())
+		{
+			Direction dir = bestDirectionFlex(pos,unit(myUnits[u]).position(),diag);
+			orders[u] = MyOrder(Order(u,dir,OrderType::movement),randomNumber(PRIO_FETCH,PRIO_FETCH+5));
+
+			targets.target(pos);
+			availableUnits[u] = false;
+			finished = true;
+		}
+
+		if(finished)
+		{
+			return true;
+		} 
+	}while(not info.q.empty());
+	return false;
+}
 
 
 
@@ -382,6 +674,28 @@ void doOpeningMoves()
 	UTILITY
 
 */
+
+template <typename T>
+int dichSearch(const T& thing, const std::vector<T>& vec)
+{
+	return recursiveDichSearch(0,vec.size()-1,thing,vec);
+}
+
+template <typename T>
+int recursiveDichSearch(int l, int r, const T& thing, const std::vector<T>& vec)
+{
+	if(r>l) return -1;
+	int m = (l+r)/2;
+	if(vec[m] > thing){
+		return recursiveDichSearch(l,m-1,thing,vec);
+	}
+	else if(vec[m] < thing){
+		return recursiveDichSearch(m+1,r,thing,vec);
+	}
+	return m;
+}
+
+bool isDiagonal(Direction d){return d >= Direction::UL;}
 
 /*
 	Virtualizes a position as if you're down-left
@@ -432,6 +746,46 @@ static Direction bestDirection(Position desired, Position act){
   else if(desired.y > act.y){ return Direction::right; }
   return Direction::left;
 }
+static Direction decide(Position pos){
+  if(pos.x < 0 and pos.y < 0){
+    int rand = randomNumber(0,1);
+    if(rand == 0){return Direction::up;}
+    return Direction::left;
+  }
+  else if(pos.x > 0 and pos.y > 0){
+    int rand = randomNumber(0,1);
+    if(rand == 0){return Direction::down;}
+    return Direction::right;
+  }
+  else if(pos.x == 0){
+    if(pos.y < 0){
+      return Direction::left;
+    }
+    return Direction::right;
+  }
+  else if(pos.y == 0){
+    if(pos.x < 0){
+      return Direction::up;
+    }
+    return Direction::down;
+  }
+  else if(pos.x < 0){
+    int rand = randomNumber(0,1);
+    if(rand == 0){return Direction::up;}
+    return Direction::right;
+  }
+  else if(pos.y < 0){
+    int rand = randomNumber(0,1);
+    if(rand == 0){return Direction::down;}
+    return Direction::left;
+  }
+  return Direction::null;
+}
+
+static Direction bestDirectionFlex(Position desired, Position act, bool diag = false){
+  if(diag) return bestDirection(desired,act);
+  return decide(Position(desired.x-act.x,desired.y-act.y));
+}
 
 /*
 	Returns a targeteable square in melee range according to the found function criteria
@@ -454,6 +808,35 @@ Position meleeTarget(Position act, bool diag, bool(*found)(const Square&))
   return Position(-1,-1);
 }
 
+void queueAdjacentPositions(BFSNode info, queue<BFSNode>& toVisit, PositionSet& visited, bool tryDiagonal = false, int plId = -1)
+{
+  vector<Direction> permutation;
+
+  if(tryDiagonal){
+    if(plId == -1){cerr << "using tryDiagonal with plId == -1" << endl; exit(1);}
+    permutation.reserve(8);
+    vector<int> perm = randomPermutation();
+    for(int i = 0; i < perm.size(); ++i){
+      permutation.emplace_back(DIRS_DIAGONAL[perm[i]]);
+    }
+  }
+  else{
+    permutation.reserve(4);
+  }
+
+  vector<int> permNormal = randomPermutation();
+  for(int i = 0; i < permNormal.size(); ++i){
+    permutation.emplace_back(DIRS_STRAIGHT[permNormal[i]]);
+  }
+  for(int i = 0; i < permutation.size(); ++i){
+    Position aux = info.pos+permutation[i];
+    if(posOk(aux) and not visited.queued(aux) and not visited.contains(aux) ){
+      if(isDiagonal(permutation[i]) and square(aux).painter() != plId) continue;
+      toVisit.push(BFSNode(info.dist+1,aux));
+      visited.queue(aux);
+    }
+  }
+}
 
 
 /*
@@ -465,3 +848,5 @@ static bool playerHasBubble(const Square& sq){ return sq.hasBubble(); }
 //static bool playerHasBubble(const BFSStruct& str){ return str.sq.hasBubble(); }
 
 }; //Struct end
+
+REGISTER_PLAYER(PLAYER_NAME);
