0bac1cbb2b317e93c038ac428f57ac07eb609ca2
Jon <jon.campillo@gmail.com>
2024-02-23 22:53:19 +0100

    can check time anytime

--- src/game/Timer.hh
+++ src/game/Timer.hh
@@ -19,6 +19,17 @@ public:
     Stop();
   }
 
+  double getTime() const
+  {
+    auto nowTimepoint = std::chrono::high_resolution_clock::now();
+    auto start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
+    auto end = std::chrono::time_point_cast<std::chrono::microseconds>(nowTimepoint).time_since_epoch().count();
+    auto duration = end-start;
+    double ms = duration*0.001;
+
+    return ms;
+  }
+
   void Stop()
   {
     auto endTimepoint = std::chrono::high_resolution_clock::now();
